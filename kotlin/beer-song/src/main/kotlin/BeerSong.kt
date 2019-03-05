f72a0e91c988497728aa8d25c93b53a368b7f72f
Jaryn Colbert <jaryn.colbert@gmail.com>
2019-03-05 18:55:21 -0500

    Beer Song 2.4 WIP - Bar should be abstract

--- kotlin/beer-song/src/main/kotlin/BeerSong.kt
+++ kotlin/beer-song/src/main/kotlin/BeerSong.kt
@@ -1,5 +1,51 @@
 class BeerSong {
 
+    private open class Bar(var numBeers: Int,
+                           val transitionMessage: String = "Take one down and pass it around") {
+
+        open fun bottles(): String {
+            return numBeers.toString() + " " + contents()
+        }
+
+        open fun contents(): String {
+            return "bottles"
+        }
+
+        open fun takeOne(): Bar {
+            if (numBeers == 2) {
+                return SingleBeerBar()
+            } else {
+                numBeers -= 1
+                return this
+            }
+        }
+    }
+
+    private class EmptyBar : Bar(0, "Go to the store and buy some more") {
+        override fun bottles(): String {
+            return "no more"
+        }
+
+        override fun takeOne(): Bar {
+            return restock()
+        }
+
+        fun restock(): Bar {
+            return Bar(99)
+        }
+    }
+
+    private class SingleBeerBar : Bar(1, "Take it down and pass it around") {
+        override fun contents(): String {
+            return "bottle"
+        }
+
+        override fun takeOne(): Bar {
+            return EmptyBar()
+        }
+    }
+
+
     companion object {
 
         fun verses(start: Int, stop: Int): String {
@@ -8,29 +54,14 @@ class BeerSong {
             return (start.downTo(stop)).joinToString("\n") {verse(it) }
         }
 
-        private fun verse(round: Int): String {
-            val transitionMsg = when (round) {
-                0 -> "Go to the store and buy some more"
-                1 -> "Take it down and pass it around"
-                else -> "Take one down and pass it around"
-            }
-            val bottles = getBottleRef(round)
+        private fun verse(numBeers: Int): String {
+            val bar = Bar(numBeers)
+            val bottles = bar.bottles()
 
             return "${bottles.capitalize()} of beer on the wall, $bottles of beer.\n" +
-                    "$transitionMsg, ${getBottleRef(round - 1)} of beer on the wall.\n"
+                    "${bar.transitionMessage}, ${bar.takeOne().bottles()} of beer on the wall.\n"
         }
 
-        private fun getBottleRef(round: Int): String {
-            return when (round) {
-                -1 -> "99"
-                0 -> "no more"
-                else -> round.toString()
-            } + " " + pluralizeBottles(round)
-        }
-
-        private fun pluralizeBottles(round: Int): String {
-            return if (round == 1) "bottle" else "bottles"
-        }
     }
 
 }
