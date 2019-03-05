489b93af9c256e13d7a8fc891c4eba5fc06ee861
Jaryn Colbert <jaryn.colbert@gmail.com>
2019-03-05 16:03:10 -0500

    Diamond 2.2 - Final soln, tests passing

--- kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
+++ kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
@@ -1,21 +1,23 @@
 class DiamondPrinter {
 
 
-    fun printToList(char: Char): List<String> {
-        val alphabet = 'A'..'Z'
-        val charIndex = alphabet.indexOf(char)
+    fun printToList(targetChar: Char): List<String> {
+        val targetIndex = ('A'..'Z').indexOf(targetChar)
 
-        return ('A'..char).mapIndexed { alphabetIndex, c ->
-            val spaceCount = charIndex - alphabetIndex
-            val outerSpaces = " ".repeat(spaceCount)
+        val top = ('A'..targetChar).mapIndexed { currIndex, c ->
+            val outerSpaces = " ".repeat(targetIndex - currIndex)
 
-            if (alphabetIndex == 0) {
+            if (currIndex == 0) {
                 outerSpaces + c + outerSpaces
             } else {
-                val innerSpaces = " ".repeat(alphabetIndex * 2 - 1)
+                val innerSpaces = " ".repeat(currIndex * 2 - 1)
                 outerSpaces + c + innerSpaces + c + outerSpaces
             }
         }
+
+        val bottom = top.dropLast(1).reversed()
+
+        return top + bottom
 
     }
 
