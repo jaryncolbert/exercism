8171926d50c2fe78db88d949b7cdc6c1aa13e4b1
Jaryn Colbert <jaryn.colbert@gmail.com>
2019-03-06 14:34:56 -0500

    Gigasecond #4 - Completed, tests passing

--- kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
+++ kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
@@ -1,13 +1,21 @@
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+import java.time.temporal.Temporal
 
-class Gigasecond(birthDate: LocalDate) {
+class Gigasecond(birthDate: Temporal) {
 
-    val date: LocalDate
+    val date: LocalDateTime
 
     init {
-        val gigasecond = 1000000000.toDouble()
-        val days = gigasecond / 60 / 60 / 24
 
-        date = birthDate.plusDays(days.toLong())
+        val birthDateTime = when (birthDate) {
+            is LocalDateTime -> birthDate
+            is LocalDate -> birthDate.atStartOfDay()
+            else -> throw IllegalArgumentException("Input must be LocalDate or LocalDateTime.")
+        }
+
+        date = birthDateTime.plus(1000000000.toLong(), ChronoUnit.SECONDS)
+
     }
 }
